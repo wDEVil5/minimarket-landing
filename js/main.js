e486649a003d7f647fea4206f3ea2aaea4625086
wilnesdevil <wilnesdevil9@gmail.com>
2026-06-04 17:22:18 -0400

    Agregar validacion del formulario de cotizacion con JavaScript

--- js/main.js
+++ js/main.js
@@ -9,3 +9,41 @@ boton.addEventListener("click", function() {
     //al hacer click agregamos o quitamos la clase activa
     menu.classList.toggle("activo");
 });
+
+// validacion del formulario
+
+//agarramos el formulario
+const formulario = document.querySelector(".formulario");
+
+//escuchamos el evento del submit
+formulario.addEventListener("submit", function (event) {
+    //evitamos que la pagina se recargue
+    event.preventDefault();
+
+    //obtenemos los valores que escribio el usuario
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const correo = document.getElementById("correo").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    //validaciones
+    if (nombre === "") {
+        alert ("porfavor, escribe tu nombre");
+        return;
+    }
+
+    if (!correo.includes("@") || !correo.includes(".")) {
+        alert("porfavor escribe un coreo valido.");
+        return;
+    }
+
+    if (mensaje.length < 10) {
+        alert("cuentanos un poco mas sobre lo que necesitas (minino 10 caracteres)");
+        return;
+    }
+
+    //si todo se encuentra bien
+    alert("Gracias!, Tu solicirud fue enviada correctamente");
+    formulario.reset();
+
+});
